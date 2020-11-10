61a453b01ff250cfe25ad6eb35d617e17a056f65
Serg_VM <mitin-71@yandex.ru>
2020-11-10 14:38:38 +0400

    On branch Lesson_6  Changes to be committed: 	modified:   Lesson_6/Lesson_5_scrapy/pipelines.py

--- Lesson_6/Lesson_5_scrapy/pipelines.py
+++ Lesson_6/Lesson_5_scrapy/pipelines.py
@@ -9,26 +9,49 @@ from itemadapter import ItemAdapter
 from pymongo import MongoClient
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy import Request
+import json
+from scrapy.exporters import PythonItemExporter
+
 
 class Lesson5ScrapyPipeline:
+
     def __init__(self):
-        client = MongoClient()
-        self.db = client['parse_20']
-        pass
+        # client = MongoClient()
+        # self.db = client['parse_20']
+        self.f = open("insta_images.json", "w+", encoding='utf-8')
+#        self.f = open("insta_user_and_follows.json", "w+", encoding='utf-8')
+
+    def __del__(self):
+        self.f.close()
+
+    def _get_exporter(self, **kwargs):
+        return PythonItemExporter(binary=False, **kwargs)
 
     def process_item(self, item, spider):
         item_name = type(item).__name__
         print( f' Lesson5ScrapyPipeline => process_item with type_name:  \'{item_name}\'')
-        connection = self.db[item_name]
-        connection.insert_one(item)
+        # connection = self.db[item_name]
+        # connection.insert_one(item)
+        ie = self._get_exporter()
+        exported = ie.export_item(item)
+        #print(json.dumps(exported))
+        try:
+            self.f.write(json.dumps(exported))
+        except IOError as err:
+            print(f"Outpur to insta_images.json ERROR: {err}")
         return item
 
 
 class Lesson5ScrapyImagePipeline(ImagesPipeline):
 
-
     def get_media_requests(self, item, info):
-        images = item.get('img', item['data'].get('profile_pic_url', item['data'].get('display_url', [])))
+        images = item.get('img',
+                          item.get('data', {}).get('profile_pic_url',
+                                                   item.get('data', {}).get('display_url',
+                                                                            []
+                                                                            )
+                                                   )
+                          )
         if not isinstance(images, list):
             images = [images]
         for url in images:
@@ -42,3 +65,4 @@ class Lesson5ScrapyImagePipeline(ImagesPipeline):
             item['img'] = [itm[1] for itm in results if itm[0]]
         except KeyError:
             pass
+        return item
